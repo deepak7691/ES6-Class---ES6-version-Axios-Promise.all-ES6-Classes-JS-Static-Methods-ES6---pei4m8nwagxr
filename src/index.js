157a777bd78371b81118ea30303033b6a3055e08
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2022-12-29 19:52:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-
-
-module.exports = { API }
+class API {
+    #secure = false;
+  
+    constructor(url, method = 'GET') {
+      this.url = url;
+      this.method = method;
+      this.#secure = this.url.startsWith('https');
+    }
+  
+    isSecure() {
+      return this.#secure;
+    }
+  
+    updateURL(url) {
+      this.url = url;
+      this.#secure = this.url.startsWith('https');
+    }
+  }
+  
+  const s = new API('http://api.com/api/hello');
+  console.log(s.isSecure()); 
+  
+  s.updateURL('https://api.com/api/hello');
+  console.log(s.isSecure()); 
+  
+  console.log(s.url); 
+  console.log(s.method);
+  console.log(s.secure); 
