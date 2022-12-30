ecf157e7944d4196d5f2b891fcd92acd39c02224
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2022-12-30 17:08:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,17 +17,3 @@ class API {
     }
   }
   
-
-const s = new API('http://api.com/api/hello')
-console. log(s. isSecure()) // false
-
-s.updateUrl('https://api.com/api/hello')
-console. log(s.isSecure()) // true
-
-console.log(s.url) // https://api. com/api/hello
-
-console.log(s.method) // GET
-
-console.log(s.secure) // undefined (because private field)
-
-  
